b2ff8b891328570467d7d59f220556126a3f8496
Shashank Avvaru <alvshashank@gmail.com>
2026-03-01 21:26:20 +0530

    Prepared release 1.0

--- src/server.js
+++ src/server.js
@@ -3,11 +3,11 @@ const app = express();
 const login = require('./login/login');
 
 app.get('/', (req, res) => {
-  res.send('<h1>Login Feature Branch</h1>');
+  res.send('<h1>Release 1.0 Stable</h1>');
 });
 
 app.get('/login', login);
 
 app.listen(3000, () => {
-  console.log('Login feature running');
+  console.log('Release 1.0 running');
 });
