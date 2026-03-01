f39a4fcd83e14abae6d63ccd8c13bd3a40c1db07
Shashank Avvaru <alvshashank@gmail.com>
2026-03-01 21:27:01 +0530

    Applied hotfix 0.2

--- src/server.js
+++ src/server.js
@@ -3,11 +3,11 @@ const app = express();
 const login = require('./login/login');
 
 app.get('/', (req, res) => {
-  res.send('<h1>Release 1.0 Stable</h1>');
+  res.send('<h1>Hotfix 0.2 Applied</h1>');
 });
 
 app.get('/login', login);
 
 app.listen(3000, () => {
-  console.log('Release 1.0 running');
+  console.log('Hotfix running');
 });
